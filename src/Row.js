2717de54d272f2ee29a6be8d4c4f10da739b414c
Shubham <shubham.jhamb2000@gmail.com>
2022-04-10 17:48:48 -0400

    commit on master

--- src/Row.js
+++ src/Row.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
 // the import is default so we can use any name instead of the 'instance' we exported in axios.
 const base_url = "https://image.tmdb.org/t/p/original/";
 // for the images, can be found in the tmdb documentation
 
 const Row = ({ title, fetchURL, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // we need a snippet of code which runs based on a specific condition/variable
   useEffect(() => {
@@ -21,6 +25,27 @@ const Row = ({ title, fetchURL, isLargeRow }) => {
     fetchData();
   }, [fetchURL]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    console.log(movie);
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <div className="row">
       {/* title */}
@@ -34,6 +59,7 @@ const Row = ({ title, fetchURL, isLargeRow }) => {
             //   renders the entire row, it just renders what needs to be rendered
             // this is an optimization step
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -42,6 +68,8 @@ const Row = ({ title, fetchURL, isLargeRow }) => {
           />
         ))}
       </div>
+      {console.log(trailerUrl)}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
